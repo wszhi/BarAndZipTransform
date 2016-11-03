b9b7c44a6f68d17d119b18f4a6e29065c2a8ffa9
Shengzhi Wang <szwang@thoughtworks.com>
2016-11-03 15:31:45 +0800

    delete files

--- java/maven/src/main/java/TranslateBar2ZipService.java
+++
@@ -1,5 +0,0 @@
-public class TranslateBar2ZipService {
-    public CoreResult translate(String barcode) {
-        return null;
-    }
-}
--- java/maven/src/main/java/TranslateZip2BarService.java
+++
@@ -1,5 +0,0 @@
-public class TranslateZip2BarService {
-    public CoreResult translate(String zipcode) {
-        return null;
-    }
-}
